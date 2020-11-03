a0b86870b068e7c30591062b6e28e8006a45c587
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-11-02 23:12:37 -0300

    Seção 12:Testando aplicação real / 66. Teste de Contas

--- BarrigaRest/src/main/java/br/ce/fcdmma/rest/tests/refactory/ContasTest.java
+++ BarrigaRest/src/main/java/br/ce/fcdmma/rest/tests/refactory/ContasTest.java
@@ -1,15 +1,18 @@
 package br.ce.fcdmma.rest.tests.refactory;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+import br.ce.fcdmma.rest.core.BaseTest;
 import io.restassured.RestAssured;
 
-public class ContasTest {
+public class ContasTest extends BaseTest{
 
 	@BeforeClass
 	public static void login() {
@@ -22,5 +25,47 @@ public class ContasTest {
 		RestAssured.requestSpecification.header("Authorization", "JWT " + TOKEN);
 		
 		RestAssured.get("/reset").then().statusCode(200);
+	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		given()
+			.body("{\"nome\": \"Conta inserida\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		Integer CONTA_ID = getIdContaPeloNome("Conta para alterar");
+		
+		given()
+			.body("{\"nome\": \"Conta Alterada\"}")
+			.pathParam("id", CONTA_ID)
+		.when()
+			.put("/contas/{id}")
+		.then()
+			.statusCode(200)
+			.body("nome", is("Conta Alterada"))
+		;
+	}
+	
+	@Test
+	public void naoDeveInserirContaComMesmoNome() {
+		given()
+			.body("{\"nome\": \"Conta mesmo nome\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(400)
+			.body("error", is("Já existe uma conta com esse nome!"))
+		;
+	}
+	
+	public Integer getIdContaPeloNome(String nome) {
+		return RestAssured.get("/contas?nome=" + nome).then().extract().path("id[0]");
 	}
 }
